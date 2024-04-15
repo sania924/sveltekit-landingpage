8f19013f6d502b3f3c8b25f76fcfe8489323b303
sania924 <saniayasin924@gmail.com>
2024-04-15 20:25:25 +0500

    landingpage in svltekit

---
+++ src/components/Product.svelte
@@ -0,0 +1,21 @@
+<script>
+     import SectionWrapper from "./SectionWrapper.svelte";
+</script>
+<SectionWrapper id="product">
+    <div
+    class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14"
+>
+    <div class="flex flex-col gap-2">
+        <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
+            Start your lifetime <span class="text-indigo-400"
+                >gym streak</span
+            > today
+        </p>
+        <h3
+            class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
+        >
+            Features For Everyone
+        </h3>
+    </div>
+</div>
+</SectionWrapper>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Hero from "../components/Hero.svelte";
+    // import Product from "../components/Product.svelte";
+    import Conversion from "../components/Conversion.svelte";
+    import Footer from "../components/Footer.svelte";
+</script>
+<main class="flex flex-col">
+    <Hero></Hero>
+    <!-- <Product/> -->
+    <Conversion/>
+    <Footer/>
+</main>
